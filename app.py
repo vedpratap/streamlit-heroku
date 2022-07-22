7a5456991807da920ac27c5b4d3b4b5332e9db5f
Ved Pratap <96064441+vedpratap@users.noreply.github.com>
2022-07-22 11:34:27 +0530

    upload

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+
+st.write("# Addition of two given numbers")
+
+st.header('User Input Parameters')
+
+def user_input_features():
+    first_number = st.number_input("first_number")
+    second_number = st.number_input("second_number")
+    data = {'first_number': first_number,'second_number': second_number}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+st.subheader('Result')
+sum = df['first_number']+df['second_number']
+st.write("The sum of given 2 numbers is", sum[0])
