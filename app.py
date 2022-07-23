ba037b37724f5202d1993ad349c46a13b283c7c9
Ved Pratap <96064441+vedpratap@users.noreply.github.com>
2022-07-23 11:55:04 +0530

    Update

--- app.py
+++ app.py
@@ -5,11 +5,9 @@ st.write("# Addition of two given numbers")
 
 st.header('User Input Parameters')
 
-first_number = st.number_input("first_number", '%d')
-second_number = st.number_input("second_number" '%d')
-st.write(first_number)
+first_number = st.number_input("first_number")
+second_number = st.number_input("second_number")
  
-
 st.subheader('Result')
 #sum = first_number+second_number
 st.write("The sum of given 2 numbers is", first_number+second_number)
