ae8d521551e881ce8c211f40c1b43265c6cb007c
Ved Pratap <96064441+vedpratap@users.noreply.github.com>
2022-07-23 11:38:48 +0530

    update

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ import pandas as pd
 st.write("# Addition of two given numbers")
 
 st.header('User Input Parameters')
-def user_input_features():
- first_number = st.number_input("first_number")
- second_number = st.number_input("second_number")
- return (first_number + second_number)
+
+first_number = st.number_input("first_number")
+second_number = st.number_input("second_number")
+st.write(first_number)
  
-df = user_input_features()
+
 st.subheader('Result')
 #sum = first_number+second_number
-st.write("The sum of given 2 numbers is", df)
+st.write("The sum of given 2 numbers is", first_number+second_number)
