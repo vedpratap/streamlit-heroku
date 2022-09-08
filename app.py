4b7a45165d747897e503e2f9819f589929c8226c
Ved Pratap <96064441+vedpratap@users.noreply.github.com>
2022-09-08 22:19:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,6 @@ second_number = st.number_input("second_number")
 st.subheader('Result')
 sum = first_number+second_number
 st.write('The sum of given 2 numbers is', sum)
+st.write()
+st.write()
+st.write('Created by Ved Pratap')
